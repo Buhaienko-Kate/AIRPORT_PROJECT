7737d70007324ecd2ed40b4711c305e3cb8a8013
Buhaienko-Kate <buguzia89@gmail.com>
2021-12-03 19:07:40 +0200

    change flight board

--- src/flights/components/flightinfo/FlightBoard.jsx
+++ src/flights/components/flightinfo/FlightBoard.jsx
@@ -2,12 +2,13 @@ import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import qs from 'qs';
+import moment from 'moment';
 import { useParams, useLocation } from 'react-router-dom';
-import { showFilteredListSelector } from '../../flights.selectors';
+import { showFlightsListSelector } from '../../flights.selectors';
 import * as flightsAction from '../../flights.actions';
 import FlightBoardBody from './FlightBoardBody';
 
-const FlightBoard = ({ flightData, getFlightList, showFlightNumber }) => {
+const FlightBoard = ({ flightData, getFlightList, showFlightNumber, showFlightData }) => {
   const { direction } = useParams();
 
   const flightNumber = qs.parse(useLocation().search, {
@@ -22,7 +23,37 @@ const FlightBoard = ({ flightData, getFlightList, showFlightNumber }) => {
   let isFlights;
 
   if (flightData && flightData.length > 0) {
-    isFlights = flightData.map(flight => <FlightBoardBody key={flight.id} {...flight} />);
+    isFlights = flightData
+      .map(flight => {
+        const destination = flight['airportToID.name'] || flight['airportFromID.name'];
+
+        const statusName = direction === 'departures' ? 'Took of' : 'Landed';
+
+        const statusTime =
+          flight.status === 'DP'
+            ? moment(flight.timeDepFact).format('h:mm')
+            : moment(flight.timeLandFact).format('h:mm');
+
+        const status = `${statusName} ${statusTime}`;
+
+        return {
+          id: flight.ID,
+          terminal: flight.term,
+          schedule: moment(flight.timeToStand).format('h:mm'),
+          destination,
+          status,
+          companyLogoSource: flight.airline.en.logoSmallName,
+          companyName: flight.airline.en.name,
+          flightNum: flight.codeShareData[0].codeShare,
+        };
+      })
+      .filter(flight => {
+        if (!flightNumber) {
+          return flight;
+        }
+        return flight.flightNum.toLowerCase().includes(flightNumber.toLowerCase());
+      })
+      .map(flight => <FlightBoardBody key={flight.id} {...flight} />);
   } else {
     isFlights = (
       <tr className="not-found">
@@ -60,13 +91,14 @@ FlightBoard.propTypes = {
 
 const mapState = state => {
   return {
-    flightData: showFilteredListSelector(state),
+    flightData: showFlightsListSelector(state),
   };
 };
 
 const mapDispatch = {
   getFlightList: flightsAction.getFlightList,
   showFlightNumber: flightsAction.showFlightNumber,
+  showFlightData: flightsAction.showFlightData,
 };
 
 export default connect(mapState, mapDispatch)(FlightBoard);
